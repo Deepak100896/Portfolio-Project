e420bac0bc34b217aa8b7c2e54480b0a91d12133
DeepakGupta <Deepakgupta100896@gmail.com>
2020-04-29 00:18:17 +0530

    Added models

--- Jobs/models.py
+++ Jobs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+# Model object will be saved in the database with the below line of code
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')
+    #ImageField inherits from FileField according to Django documentation
+    summary = models.CharField(max_length =200)
